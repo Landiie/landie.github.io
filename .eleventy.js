e26cbd29474a94110d9c014e0bad763999490566
Landie <landsurfer2000@gmail.com>
2023-08-25 07:40:18 -0700

    getting farther

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
 module.exports = function (eleventyConfig) {
+  //adds ids to headers
+  const anchors_plugin = require("@orchidjs/eleventy-plugin-ids")
+  eleventyConfig.addPlugin(anchors_plugin)
+
+  eleventyConfig.addPassthroughCopy("./src/pfp.png")
   eleventyConfig.addPassthroughCopy("./src/styles.css")
+  eleventyConfig.addPassthroughCopy("./src/styles-page.css")
+  eleventyConfig.addPassthroughCopy("./src/papaparse.min.js")
   eleventyConfig.addPassthroughCopy("./src/shop/shop.js")
   eleventyConfig.addPassthroughCopy("./src/shop/shop-live-indicator.js")
   eleventyConfig.addPassthroughCopy("./src/assets")
--- mdfm-to-json.js
+++ mdfm-to-json.js
@@ -1,74 +1,61 @@
-const fs = require("fs");
-const crypto = require("crypto");
-const path = require("path");
+const fs = require("fs")
+const crypto = require("crypto")
+const path = require("path")
 
-const directoryPath = "/src/shop/sammi-extensions"; // input .md files
-const outputDirectory = "/src/shop/sammi-extensions"; // output .json files
+const directoryPath = "src/sammi-extensions/" // input .md files
+const outputDirectory = "src/shop/sammi-extensions" // output .json files
 
-const getHash = content => {
-  const hash = crypto.createHash("md5");
-  const data = hash.update(content, "utf-8");
-  const gen_hash = data.digest("hex");
+const getHash = (content) => {
+  const hash = crypto.createHash("md5")
+  const data = hash.update(content, "utf-8")
+  const gen_hash = data.digest("hex")
 
-  return gen_hash;
-};
-
-const trimExt = file => {
-  let fileNoExt = file.replace(".md", "");
-  fileNoExt = fileNoExt.replace("-", "_");
-
-  return fileNoExt;
-};
-
-const getJsonData = mdContent => {
-  const posEnd = mdContent.indexOf(`\n---`);
-  const jsonData = mdContent.substring(9, posEnd);
-
-  return jsonData;
-};
-
-const createDocsDir = productName => {
-  let dirName = productName.replace(".md", "");
-  if (!fs.existsSync(path.join(directoryPath, dirName), "utf-8")) {
-    fs.mkdirSync(path.join(directoryPath, dirName), "utf-8");
-    console.log(`directory made: ${path.join(directoryPath, dirName)}`)
-  }
+  return gen_hash
 }
 
+const trimExt = (file) => {
+  let fileNoExt = file.replace(".md", "")
+  fileNoExt = fileNoExt.replace("-", "_")
 
-if (!fs.existsSync(path.join(directoryPath, dirName), "utf-8")) {
-  fs.mkdirSync(path.join(directoryPath, dirName), "utf-8");
+  return fileNoExt
 }
 
-fs.readdirSync(directoryPath).forEach(file => {
-  if (file.endsWith(".md")) {
-    let needsWrite = true;
+const getJsonData = (mdContent) => {
+  const posEnd = mdContent.indexOf(`\n---`)
+  const jsonData = mdContent.substring(9, posEnd)
 
-    const mdContent = fs.readFileSync(path.join(directoryPath, file), "utf-8");
-    const fileNoExt = trimExt(file);
-    const jsonFileName = `${fileNoExt}_info.json`;
-    const jsonData = getJsonData(mdContent);
-    
-    const jsonExists = fs.existsSync(path.join(outputDirectory, jsonFileName));
-    console.log(jsonExists);
+  return jsonData
+}
 
-    if (jsonExists) {
-      const jsonContent = fs.readFileSync(
-        path.join(outputDirectory, jsonFileName),
-        "utf-8"
-      );
-      const jsonExistingHash = getHash(jsonContent);
-      const jsonNewHash = getHash(jsonData);
-      console.log(jsonExistingHash);
-      console.log(jsonNewHash);
+fs.readdirSync(directoryPath).forEach((file) => {
+  if (file.endsWith("docs.md") || !file.endsWith(".md")) return
 
-      if (jsonNewHash == jsonExistingHash) needsWrite = false;
-    }
-    if (needsWrite) {
-      console.log(`${jsonFileName} needs writing!`);
-      fs.writeFileSync(path.join(outputDirectory, jsonFileName), jsonData);
-    } else {
-      console.log(`No write for ${jsonFileName}`);
-    }
+  let needsWrite = true
+
+  const mdContent = fs.readFileSync(path.join(directoryPath, file), "utf-8")
+  const fileNoExt = trimExt(file)
+  const jsonFileName = `${fileNoExt}_info.json`
+  const jsonData = getJsonData(mdContent)
+
+  const jsonExists = fs.existsSync(path.join(outputDirectory, jsonFileName))
+  console.log(jsonExists)
+
+  if (jsonExists) {
+    const jsonContent = fs.readFileSync(
+      path.join(outputDirectory, jsonFileName),
+      "utf-8"
+    )
+    const jsonExistingHash = getHash(jsonContent)
+    const jsonNewHash = getHash(jsonData)
+    console.log(jsonExistingHash)
+    console.log(jsonNewHash)
+
+    if (jsonNewHash == jsonExistingHash) needsWrite = false
   }
-});
+  if (needsWrite) {
+    console.log(`${jsonFileName} needs writing!`)
+    fs.writeFileSync(path.join(outputDirectory, jsonFileName), jsonData)
+  } else {
+    console.log(`No write for ${jsonFileName}`)
+  }
+})
