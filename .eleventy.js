edffed70ab046ca80e66fa78d557be76df59494c
Landie <43101146+Landiie@users.noreply.github.com>
2023-08-30 02:37:56 -0700

    product

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,34 @@
 module.exports = function (eleventyConfig) {
   //adds ids to headers
-  const anchors_plugin = require("@orchidjs/eleventy-plugin-ids")
-  eleventyConfig.addPlugin(anchors_plugin)
+  const anchors_plugin = require("@orchidjs/eleventy-plugin-ids");
+  eleventyConfig.addPlugin(anchors_plugin);
 
-  eleventyConfig.addPassthroughCopy("./src/pfp.png")
-  eleventyConfig.addPassthroughCopy("./src/styles.css")
-  eleventyConfig.addPassthroughCopy("./src/styles-page.css")
-  eleventyConfig.addPassthroughCopy("./src/papaparse.min.js")
-  eleventyConfig.addPassthroughCopy("./src/shop/shop.js")
-  eleventyConfig.addPassthroughCopy("./src/shop/shop-live-indicator.js")
-  eleventyConfig.addPassthroughCopy("./src/assets")
-  eleventyConfig.addPassthroughCopy("./src/site.webmanifest")
-  eleventyConfig.addPassthroughCopy("./src/favicon.ico")
-  eleventyConfig.addPassthroughCopy("./src/patrons.json")
-  eleventyConfig.addPassthroughCopy("./src/shop/sammi-extensions/*.json")
+  eleventyConfig.addPassthroughCopy("./src/pfp.png");
+  eleventyConfig.addPassthroughCopy("./src/styles.css");
+  eleventyConfig.addPassthroughCopy("./src/styles-page.css");
+  eleventyConfig.addPassthroughCopy("./src/papaparse.min.js");
+  eleventyConfig.addPassthroughCopy("./src/shop/shop.js");
+  eleventyConfig.addPassthroughCopy("./src/shop/shop-live-indicator.js");
+  eleventyConfig.addPassthroughCopy("./src/shop/product.js");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/site.webmanifest");
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("./src/patrons.json");
+  eleventyConfig.addPassthroughCopy("./src/shop/sammi-extensions/*.json");
 
   eleventyConfig.addFilter("splitContent", (content, sectionNumber) => {
-    const sections = content.split("<!-- more -->")
-    return sections[sectionNumber]
-  })
+    const sections = content.split("<!-- more -->");
+    return sections[sectionNumber];
+  });
+
+  eleventyConfig.addFilter("concat", (value1, value2) => {
+    return value1 + value2;
+  });
 
   return {
     dir: {
       input: "src",
       output: "public",
     },
-  }
-}
+  };
+};
--- src/shop/shop.js
+++ src/shop/shop.js
@@ -1,81 +1,81 @@
-const shopItems = document.querySelectorAll(".shop-item");
-const allLabels = document.querySelectorAll(".label-list-filter > button");
-let activeFilters = [];
+// const shopItems = document.querySelectorAll(".shop-item");
+// const allLabels = document.querySelectorAll(".label-list-filter > button");
+// let activeFilters = [];
 
-shopSearch();
+// shopSearch();
 
-document.querySelector(".shop-search").addEventListener("input", function (e) {
-  console.log(e);
-  shopSearch(e);
-});
+// document.querySelector(".shop-search").addEventListener("input", function (e) {
+//   console.log(e);
+//   shopSearch(e);
+// });
 
-document.querySelector(".label-list-filter").addEventListener("click", function (e) {
-  const label = e.target.classList[0];
-  const pos = activeFilters.indexOf(label);
-  console.log(pos)
-  if (label !== "label-list-filter") { //prevents user from clicking wrapper
-    //console.log(e);
-    if (pos === -1) {
-      activeFilters.push(label)
-    } else {
-      console.log("Label already selected, removing")
-      activeFilters.splice(pos, 1);
-    }
-    //console.log(activeFilters)
-    applyFilters()
-  }
-});
+// document.querySelector(".label-list-filter").addEventListener("click", function (e) {
+//   const label = e.target.classList[0];
+//   const pos = activeFilters.indexOf(label);
+//   console.log(pos)
+//   if (label !== "label-list-filter") { //prevents user from clicking wrapper
+//     //console.log(e);
+//     if (pos === -1) {
+//       activeFilters.push(label)
+//     } else {
+//       console.log("Label already selected, removing")
+//       activeFilters.splice(pos, 1);
+//     }
+//     //console.log(activeFilters)
+//     applyFilters()
+//   }
+// });
 
-function applyFilters() {
-  //remove all active filters
-  allLabels.forEach(label => {
-    label.classList.remove("active");
-  });
-  //give active filters active class
-  activeFilters.forEach(filter => {
-    const label = document.querySelector(`.${filter}`)
-    label.classList.add("active");
-  });
-  shopSearch()
-}
+// function applyFilters() {
+//   //remove all active filters
+//   allLabels.forEach(label => {
+//     label.classList.remove("active");
+//   });
+//   //give active filters active class
+//   activeFilters.forEach(filter => {
+//     const label = document.querySelector(`.${filter}`)
+//     label.classList.add("active");
+//   });
+//   shopSearch()
+// }
 
-function shopSearch() {
-  const value =
-    document.querySelector(".shop-search").value;
-  shopItems.forEach(item => {
-    let itemName = item.dataset.shopItemName;
-    let itemLabels = [];
-    let itemLabelDivs = document.querySelector(`[data-shop-item-name="${itemName}"] > .label-list`).children
-    for(i = 0; i < itemLabelDivs.length; i++) {
-      itemLabels.push(itemLabelDivs[i].className)
-    }
-    console.log(itemLabels)
-    if (itemName != undefined) {
-      if (itemName.toLowerCase().includes(value.toLowerCase())) {
-        console.log(`Matches ${itemName}`);
-        //check if there are any active filters, else just display
-        if (activeFilters.length == 0) {
-          item.style.display = "block";
-        } else {
-          //active filters! look through each matched item's
-          //label list, and if it matches all the active filters,
-          //show em! else, hide em!
-          let hasAFilter = true;
-          activeFilters.forEach(filter => {
-            if (!itemLabels.includes(filter)) hasAFilter = false;
-          });
-          if (hasAFilter) {
-            item.style.display = "block";
-          } else {
-            item.style.display = "none";
-          }
-        }
+// function shopSearch() {
+//   const value =
+//     document.querySelector(".shop-search").value;
+//   shopItems.forEach(item => {
+//     let itemName = item.dataset.shopItemName;
+//     let itemLabels = [];
+//     let itemLabelDivs = document.querySelector(`[data-shop-item-name="${itemName}"] > .label-list`).children
+//     for(i = 0; i < itemLabelDivs.length; i++) {
+//       itemLabels.push(itemLabelDivs[i].className)
+//     }
+//     console.log(itemLabels)
+//     if (itemName != undefined) {
+//       if (itemName.toLowerCase().includes(value.toLowerCase())) {
+//         console.log(`Matches ${itemName}`);
+//         //check if there are any active filters, else just display
+//         if (activeFilters.length == 0) {
+//           item.style.display = "block";
+//         } else {
+//           //active filters! look through each matched item's
+//           //label list, and if it matches all the active filters,
+//           //show em! else, hide em!
+//           let hasAFilter = true;
+//           activeFilters.forEach(filter => {
+//             if (!itemLabels.includes(filter)) hasAFilter = false;
+//           });
+//           if (hasAFilter) {
+//             item.style.display = "block";
+//           } else {
+//             item.style.display = "none";
+//           }
+//         }
 
-      } else {
-        item.style.display = "none";
-      }
-    } else {
-      item.style.display = "none";
-    }
-  });
-}
+//       } else {
+//         item.style.display = "none";
+//       }
+//     } else {
+//       item.style.display = "none";
+//     }
+//   });
+// }
