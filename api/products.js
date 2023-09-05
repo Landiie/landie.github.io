6e0837eac60f71315880dd09d39479f8b2ab96a4
Landie <landsurfer2000@gmail.com>
2023-09-05 13:02:28 -0700

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,6 +1,6 @@
 exports.handler = async function (event, context) {
   const { category, product } = event.queryStringParameters
-  const productData = require(`./shop/${category}/${product}_info.json`)
+  const productData = require(`.src/shop/${category}/${product}_info.json`)
   //   console.log(category, product)
   return {
     statusCode: 200,
