dbd73fa14a16d255de328cf8af0153e51193c229
Landie <landsurfer2000@gmail.com>
2023-09-05 13:11:27 -0700

    man i cant see for shizzle

--- api/products.js
+++ api/products.js
@@ -1,6 +1,7 @@
 exports.handler = async function (event, context) {
   const { category, product } = event.queryStringParameters
-  const productData = require(`.src/shop/${category}/${product}_info.json`)
+  const productData = require(`./src/shop/${category}/${product}_info.json`)
+  console.log(productData)
   //   console.log(category, product)
   return {
     statusCode: 200,
