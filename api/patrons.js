e327f9fa5ea88182a6b79c3fec4d5f080ac4a431
Landie <43101146+Landiie@users.noreply.github.com>
2023-09-10 21:50:21 -0700

    patron endpoint

---
+++ api/patrons.js
@@ -0,0 +1,21 @@
+exports.handler = async function (event, context) {
+  try {
+    patronData = require(`../src/patrons.json`);
+  } catch (error) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: error,
+      }),
+    };
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      patrons: patronData,
+    }),
+  };
+};
