b40140f72c6f54ddd5e85818b4458ace88b0e8a0
Landie <43101146+Landiie@users.noreply.github.com>
2023-09-10 21:52:26 -0700

    Update patrons.js

--- api/patrons.js
+++ api/patrons.js
@@ -14,6 +14,9 @@ exports.handler = async function (event, context) {
   }
   return {
     statusCode: 200,
+    headers: {
+        "Content-Type": "application/json"
+    },
     body: JSON.stringify({
       patrons: patronData,
     }),
