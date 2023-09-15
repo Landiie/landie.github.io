478d8d84d8a1ef9dda4c2834b975b58001463eee
Landie <43101146+Landiie@users.noreply.github.com>
2023-09-15 02:10:01 -0700

    first blog post DONE, looks so good, change img later

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,29 @@
 module.exports = function (eleventyConfig) {
   //adds ids to headers
-  const anchors_plugin = require("@orchidjs/eleventy-plugin-ids")
-  const { EleventyRenderPlugin } = require("@11ty/eleventy")
-  const { DateTime } = require("luxon")
+  const anchors_plugin = require("@orchidjs/eleventy-plugin-ids");
+  const { EleventyRenderPlugin } = require("@11ty/eleventy");
+  const { DateTime } = require("luxon");
 
-  eleventyConfig.addPlugin(anchors_plugin)
-  eleventyConfig.addPlugin(EleventyRenderPlugin)
+  eleventyConfig.addPlugin(anchors_plugin);
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
 
-  eleventyConfig.addPassthroughCopy("./src/pfp.png")
-  eleventyConfig.addPassthroughCopy("./src/papaparse.min.js")
-  eleventyConfig.addPassthroughCopy("./src/assets")
-  eleventyConfig.addPassthroughCopy("./src/site.webmanifest")
-  eleventyConfig.addPassthroughCopy("./src/favicon.ico")
-  eleventyConfig.addPassthroughCopy("./src/patrons.json")
-  eleventyConfig.addPassthroughCopy("./src/shop/sammi-extensions/*.json")
+  eleventyConfig.addPassthroughCopy("./src/pfp.png");
+  eleventyConfig.addPassthroughCopy("./src/papaparse.min.js");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/site.webmanifest");
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("./src/patrons.json");
+  eleventyConfig.addPassthroughCopy("./src/shop/sammi-extensions/*.json");
 
   //filters
   eleventyConfig.addFilter("splitContent", (content, sectionNumber) => {
-    const sections = content.split("<!-- more -->")
-    return sections[sectionNumber]
-  })
+    const sections = content.split("<!-- more -->");
+    return sections[sectionNumber];
+  });
 
   eleventyConfig.addFilter("concat", (value1, value2) => {
-    return value1 + value2
-  })
+    return value1 + value2;
+  });
 
   // eleventyConfig.addFilter("excerpt", content => {
   //   // const tokens = markdownIt.parse(content, {});
@@ -46,51 +46,83 @@ module.exports = function (eleventyConfig) {
   //   return content
   // });
 
-  eleventyConfig.addFilter("excerpt", (post) => {
-    const content = isolateParagraphs(post)
-    return content.substr(0, content.lastIndexOf(" ", 200)) + "..."
-  })
+  eleventyConfig.addFilter("excerpt", post => {
+    const content = isolateParagraphs(post);
+    return content.substr(0, content.lastIndexOf(" ", 200)) + "...";
+  });
 
-  eleventyConfig.addFilter("humanDate", (dateObj) => {
-    return dateObj.toUTCString().replace(" 00:00:00 GMT", "")
-  })
+  eleventyConfig.addFilter("humanDate", dateObj => {
+    return dateObj.toUTCString().replace(" 00:00:00 GMT", "");
+  });
 
-  eleventyConfig.addFilter("readTime", (content) => {
-    content = isolateParagraphs(content)
-    const wordsPerMinute = 200 // Average case.
-    let result
+  eleventyConfig.addFilter("readTime", content => {
+    content = isolateParagraphs(content);
+    const wordsPerMinute = 200; // Average case.
+    let result;
 
-    let textLength = content.split(" ").length // Split by words
+    let textLength = content.split(" ").length; // Split by words
     if (textLength > 0) {
-      let value = Math.ceil(textLength / wordsPerMinute)
-      result = `~${value} min read`
+      let value = Math.ceil(textLength / wordsPerMinute);
+      result = `~${value} min read`;
     }
 
-    return result
-  })
+    return result;
+  });
 
   //shortcodes
   eleventyConfig.addPairedShortcode("alert", function (content, type) {
-    let svg = ""
+    let svg = "";
     if (type == "info") {
       svg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
       <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-    </svg>`
+    </svg>`;
     } else {
       svg = `<svg xmlns="http://www.w3.org/2000/svg" class="me-2" width="32" height="32" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
       <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </svg>`
+  </svg>`;
     }
     return `<div class="alert alert-${type} d-flex flex-row justify-content-center align-items-center text-center">
     ${svg}
     <div>${content}</div>
-  </div>`
-  })
+  </div>`;
+  });
+
+  eleventyConfig.addPairedShortcode("img", function (link, alt, caption, w) {
+    if (!caption) caption = alt;
+    if (!w) w = "";
+    return `<div style="
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 1rem;
+">
+    <img src="${link}" width="${w}" alt="${alt}">
+    <i style="color: gray;">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-images" viewBox="0 0 16 16">
+            <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+            <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z"/>
+        </svg> ${caption}
+    </i>
+    <!--unused atm, for multi-image support-->
+    <nav class="d-none util-slideshow-nav">
+        <button class="btn btn-secondary util-slideshow-nav-left">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+            </svg>
+        </button>
+        <button class="btn btn-secondary util-slideshow-nav-right">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+            </svg>
+        </button>
+    </nav>
+</div>`;
+  });
 
   //functions
   function isolateParagraphs(text) {
-    text = text.replace(/(<([^>]+)>)/gi, "")
-    return text
+    text = text.replace(/(<([^>]+)>)/gi, "");
+    return text;
   }
 
   return {
@@ -99,5 +131,5 @@ module.exports = function (eleventyConfig) {
       output: "public",
     },
     markdownTemplateEngine: "njk",
-  }
-}
+  };
+};
--- src/blog/my-first-blogpost.md
+++ src/blog/my-first-blogpost.md
@@ -1,23 +1,52 @@
 ---
 title: My First Blogpost!
-date: 2023-09-11
+date: 2023-09-15
 tags: ["blog"]
 layout: blogpost.njk
 image: https://cdn.discordapp.com/attachments/1093729726054543440/1093730364847050882/Landie__femb_BG.png
 ---
 
-# WOW!
+# Hey!
 
-My first blogpost, would you look at that! I want to try many things and make sure they work well, but i dont want to spend forever on making a silly test blog post
+Welcome to my first blog post!
 
-{% alert "warning" %}
-woah
+I hope to use this to provide extended content and context to my released YouTube videos on my main channel, since I know I have more I want to talk about. I know that's not everyone's cup o' tea, so the blog posts are born!
+
+This blog post will roll out with the new backend revamp of my website, allowing, well, a blog like this to exist! I use a [Static Website Generator](https://www.cloudflare.com/learning/performance/static-site-generator/) called [Eleventy](https://www.11ty.dev/) to handle the process of bundling special text-like files into dedicated webpages, defined by a bunch of rules and layouts to carry out the task! No more typing raw HTML code to edit products on my site, or blog posts! These files then get put into a directory which the generator reads, then transforms into its own webpage. 
+
+{% img "showing a markdown file in a folder", "", "400" %}
+https://i.imgur.com/hRLsQMO.png
+{% endimg %}
+
+This blog post for example, uses a text file format called [Markdown](https://www.markdownguide.org/getting-started/#what-is-markdown) (the same as discord text!). I can generate pages so easily this way, and edit them just the same!
+
+{%img "showing example of Markdown text from this page"%}
+https://i.imgur.com/4Txdcqp.png
+{%endimg%}
+
+Whenever I save, I get a live update of my changes in real-time, It's super, super cool!
+
+Enough nerdy talk though (for now).
+
+# Why is this important?
+
+Many reasons! But the biggest reason why is because I can pump out **more products** for everyone.
+
+One of the biggest reasons why I had not updated the documentation for [LandiTube](http://landitube.landie.land) in so long is because it would take me ages if I had to edit the documentation in *raw* HTML. Markdown has the ability to include snippets of HTML, so I don't mind not being able to edit in HTML. I don't miss out on anything! for example, I can <span style="color: red;">make my text red using a span tag with a style attribute using **raw HTML** inside Markdown</span>. Not missing much!
+
+I can even use chunks of pre-defined templates to generate quick snippets of HTML.
+
+{%alert "success"%}
+This alert is made with a template!
 {%endalert%}
 
-{% alert "info" %}
-woah!
-{%endalert%}
+{%img "example of an HTML snippet", "Using eleventy shortcodes to input HTML snippets"%}
+https://i.imgur.com/7pzQ0rF.png
+{%endimg%}
 
-{% alert "success" %}
-woah!
-{%endalert%}
+Ok! That's all I wanted to say. Expect future blog posts about YouTube videos, SAMMI Extensions, SAMMI in general, OBS, Streamer tools, and so much more! I'll try to keep things in categories so you can follow **exactly what you want from me!**
+<br>
+<br>
+<br>
+<br>
+see ya~
