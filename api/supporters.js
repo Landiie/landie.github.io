31331d2abab99ac2f7798cc1298109e97a42e772
Landie <43101146+Landiie@users.noreply.github.com>
2024-04-30 01:42:04 -0700

    api fixed

--- api/supporters.js
+++ api/supporters.js
@@ -1,24 +1,27 @@
-// exports.handler = async function (event, context) {
-//   try {
-//     patronData = require(`../src/patrons.json`);
-//   } catch (error) {
-//     return {
-//       statusCode: 400,
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         message: error,
-//       }),
-//     };
-//   }
-//   return {
-//     statusCode: 200,
-//     headers: {
-//         "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({
-//       patreon: patronData
-//     }),
-//   };
-// };
+const path = require('path');
+const fs = require('fs');
+
+exports.handler = async function (event, context) {
+  let patronData
+  try {
+    patronData = fs.readFileSync(path.join(__dirname, '..', 'src', 'patrons.json'), 'utf-8');
+  } catch (error) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: error,
+      }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: patronData,
+  };
+};
--- api/test.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const fs = require("fs");
-
-exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(fs.readdirSync(path.join(__dirname, '..'))),
-  };
-};
